d5405c5c66da72aa86fb4d2751c7352aa2bf5221
Jaewook Choi <eari100@naver.com>
2023-10-12 22:51:22 +0900

    달러 곱셈 기능 테스트

---
+++ src/test/java/com/example/tdd_development_by_example/domain/DollarTests.java
@@ -0,0 +1,21 @@
+package com.example.tdd_development_by_example.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTests {
+
+    // todo:
+    // $5 + 10CHF = $10 (환율이 2:1일 경우)
+    // $5 * 2 = $10 (제일 쉬운 것)
+    // amount를 private로 만들기
+    // Dollar side effect?
+    // Money 반올림?
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
