0f10fe037b1718ca0cfa5718f49a84667442b043
Jaewook Choi <eari100@naver.com>
2023-10-12 22:54:26 +0900

    달러 곱셈 중복 기능 제거

--- src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
+++ src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
@@ -3,10 +3,10 @@ package com.example.tdd_development_by_example.domain;
 public class Dollar {
 
     Dollar(int amount) {
-
+        this.amount = 5 * 2;
     }
 
-    public int amount = 10;
+    public int amount;
 
     void times(int multiplier) {
 
