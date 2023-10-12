345ae1fa9f3722d070a05c585b6769c80278d336
Jaewook Choi <eari100@naver.com>
2023-10-12 23:16:00 +0900

    값 비교 테스트

--- src/test/java/com/example/tdd_development_by_example/domain/DollarTests.java
+++ src/test/java/com/example/tdd_development_by_example/domain/DollarTests.java
@@ -3,6 +3,7 @@ package com.example.tdd_development_by_example.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DollarTests {
 
@@ -10,7 +11,7 @@ public class DollarTests {
     // $5 + 10CHF = $10 (환율이 2:1일 경우)
     // [완료] $5 * 2 = $10
     // amount를 private로 만들기
-    // Dollar side effect?
+    // [완료] Dollar side effect?
     // Money 반올림?
     @Test
     public void testMultiplication() {
@@ -19,5 +20,10 @@ public class DollarTests {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
