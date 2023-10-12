82617614e9202670f8c514b182ae698a3ec76c47
Jaewook Choi <eari100@naver.com>
2023-10-12 23:28:21 +0900

    Dollar.amount public -> private로 수정

--- src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
+++ src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
@@ -3,7 +3,7 @@ package com.example.tdd_development_by_example.domain;
 // todo:
 // $5 + 10CHF = $10 (환율이 2:1일 경우)
 // [완료] $5 * 2 = $10
-// amount를 private로 만들기
+// [완료] amount를 private로 만들기
 // [완료] Dollar side effect?
 // Money 반올림?
 // [완료] equals
@@ -16,7 +16,7 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public int amount;
+    private int amount;
 
     // 타락한 객체?
     Dollar times(int multiplier) {
