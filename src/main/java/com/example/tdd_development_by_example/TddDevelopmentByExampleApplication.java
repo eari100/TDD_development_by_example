5631188063a16b8cabe543e2392f2cc4c0fa312f
Jaewook Choi <eari100@naver.com>
2023-10-12 22:40:01 +0900

    Initial commit

---
+++ src/main/java/com/example/tdd_development_by_example/TddDevelopmentByExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdd_development_by_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDevelopmentByExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddDevelopmentByExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tdd_development_by_example/TddDevelopmentByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdd_development_by_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDevelopmentByExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
