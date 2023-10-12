0c931605abe118dc0278e1eb5292884f045cd937
Jaewook Choi <eari100@naver.com>
2023-10-12 23:16:49 +0900

    값 비교 테스트 통과되도록 구션

--- src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
+++ src/main/java/com/example/tdd_development_by_example/domain/Dollar.java
@@ -12,4 +12,9 @@ public class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
 }
