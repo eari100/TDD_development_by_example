ed35ac9ad2e7006f0cfeb8cefa26e83fbc262825
Jaewook Choi <eari100@naver.com>
2023-10-14 13:00:04 +0900

    Money equals 오류 수정

--- src/main/java/com/example/tdd_development_by_example/domain/Money.java
+++ src/main/java/com/example/tdd_development_by_example/domain/Money.java
@@ -3,7 +3,6 @@ package com.example.tdd_development_by_example.domain;
 public class Money {
     protected int amount;
 
-    @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(money.getClass());
