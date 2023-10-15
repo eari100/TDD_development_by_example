a413423ae5ccdacbc194fe39bc1d5052d822606e
Jaewook Choi <eari100@naver.com>
2023-10-16 02:34:58 +0900

    Dollar, Franc의 팩토리 메서드 패턴을 적용

--- src/main/java/com/example/tdd_development_by_example/domain/Money.java
+++ src/main/java/com/example/tdd_development_by_example/domain/Money.java
@@ -1,10 +1,19 @@
 package com.example.tdd_development_by_example.domain;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
+    abstract Money times(int multiplier);
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
     }
 }
