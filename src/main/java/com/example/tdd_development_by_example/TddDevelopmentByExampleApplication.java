154bd0d86b577d1b6c95bb6a06bd18a954c5b6cf
Jaewook Choi <eari100@naver.com>
2023-10-22 06:50:36 +0900

    하위 클래스 제거 (Dollar, Franc)

--- src/main/java/com/example/tdd_development_by_example/TddDevelopmentByExampleApplication.java
+++ src/main/java/com/example/tdd_development_by_example/TddDevelopmentByExampleApplication.java
@@ -3,6 +3,23 @@ package com.example.tdd_development_by_example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// todo:
+// $5 + 10CHF = $10 (환율이 2:1일 경우)
+// [완료] $5 * 2 = $10
+// [완료] amount를 private로 만들기
+// [완료] Dollar side effect?
+// Money 반올림?
+// [완료] equals
+// hashcode
+// Equal null
+// Equal object
+// [완료] 5CHF x 2 = 10CHF
+// Dollar/Franc 중복
+// [완료] 공용 equals
+// [완료] 공용 times
+// [완료] Franc와 Dollar 비교하기
+// [완료] 통화?
+// [완료] testFrancMultiplication 제거
 @SpringBootApplication
 public class TddDevelopmentByExampleApplication {
 
--- src/main/java/com/example/tdd_development_by_example/domain/Franc.java
+++
@@ -1,7 +0,0 @@
-package com.example.tdd_development_by_example.domain;
-
-public class Franc extends Money {
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
